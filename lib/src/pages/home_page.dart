26a41f9e307283d4a0c6a97432dc8422a3956f01
Martin Barria Joost <martinjoostw@gmail.com>
2020-04-11 09:55:26 -0600

    button

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -5,17 +5,30 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget{
 
+final TextStyle estiloTexto = new TextStyle( fontSize: 40 );
 
 @override
 Widget build(BuildContext context) {
   return Scaffold(
     appBar: AppBar(
       title: Text('Titulo'),
+      centerTitle: true,
     ),
     body: Center(
-      child: Text('Hola Mundo'),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+        Text('Numero de clicks', style: estiloTexto),
+        Text('0', style: estiloTexto)
+      ],
+      )
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Icon( Icons.apps ),
     ),
   );
+  
 }
 
 
